44cc2931e62848bcac851165e700fcb1c565ffd2
Seth <seth.kelley1992@gmail.com>
2024-08-08 22:33:45 -0400

    RAG & Pinecone
    Added some elements to introduce RAG w/ Pinecone, just need to add data into Pinecone and query it with the embedding. Added a TODO file with some relevant things from Headstarter checklist etc.

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
+import { Pinecone } from "@pinecone-database/pinecone";
 
 const systemPrompt = `Welcome to HeadStarterAI Customer Support!
 
@@ -15,9 +16,31 @@ Understanding Results: Explanation of the feedback and results provided after yo
 Please provide details about your issue or question so I can assist you more effectively. If you need personalized support beyond what I can provide, I’ll guide you on how to reach our human support team.`
 
 export async function POST(req) {
+
+    //OpenAI & Pinecone, needs keys in .env.local file
     const openai = new OpenAI()
+    const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY })
+    const index = pc.index('headstart')
+
     const data = await req.json()
 
+    const userInput = data[data.length - 1].content
+
+    //OpenAI embedding to give to Pinecone
+    const embedding = await openai.embeddings.create({
+        model: 'text-embedding-ada-002',
+        input: userInput,
+        encoding_format: 'float'
+    })
+
+    // Checks embedding of user input and the embedding length
+    // console.log(embedding.data[0].embedding)
+    // console.log(embedding.data[0].embedding.length)
+
+    //Populate Pinecone index with data
+    //Give Pinecone the embedded user input
+
+    //Give OpenAI new data with Pinecone, not just user input
     const completion = await openai.chat.completions.create({
         messages: [
             {
